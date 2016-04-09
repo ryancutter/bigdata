fb7503686f4eba0324cc8c9bf3dbcdc9498ea582
Ryan Cutter <ryancutter@gmail.com>
2016-04-09 10:21:04 -0700

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,38 @@
+from pyspark import SparkContext
+from pyspark.sql import SQLContext, Row
+
+sc = SparkContext("local[*]", "Simple App")
+#sc = SparkContext("spark://rcutter-loaner-2.local:7077", "Simple App")
+sqlContext = SQLContext(sc)
+
+conf = {"es.resource" : "movies2/logs", "es.query" : "?q=name:bourne"}
+movies = sc.newAPIHadoopRDD("org.elasticsearch.hadoop.mr.EsInputFormat",\
+    "org.apache.hadoop.io.NullWritable", "org.elasticsearch.hadoop.mr.LinkedMapWritable", conf=conf)
+
+moviesRows = movies.map(lambda p: Row(id=int(p[1]['id']), name=p[1]['name']))
+schemaMovies = sqlContext.createDataFrame(moviesRows)
+schemaMovies.registerTempTable("movies")
+ids = sqlContext.sql("SELECT id FROM movies")
+ids.collect()
+
+conf = {"es.resource" : "acted_in/logs", "es.query" : "?q=movie_id:3480646"}
+actedIn = sc.newAPIHadoopRDD("org.elasticsearch.hadoop.mr.EsInputFormat",\
+    "org.apache.hadoop.io.NullWritable", "org.elasticsearch.hadoop.mr.LinkedMapWritable", conf=conf)
+
+actedInRows = actedIn.map(lambda p: Row(actor_id=int(p[1]['actor_id']), movie_id=int(p[1]['movie_id']), role=p[1]['role']))
+schemaActedIn = sqlContext.createDataFrame(actedInRows)
+schemaActedIn.registerTempTable("acted_in")
+ids = sqlContext.sql("SELECT actor_id FROM acted_in")
+ids.collect()
+
+conf = {"es.resource" : "actors2/logs", "es.query" : "?q=id:1203538"}
+actors = sc.newAPIHadoopRDD("org.elasticsearch.hadoop.mr.EsInputFormat",\
+    "org.apache.hadoop.io.NullWritable", "org.elasticsearch.hadoop.mr.LinkedMapWritable", conf=conf)
+
+actorsRows = actors.map(lambda p: Row(id=int(p[1]['id']), name=p[1]['name']))
+schemaActors = sqlContext.createDataFrame(actorsRows)
+schemaActors.registerTempTable("actors")
+s = "SELECT a.name AS Actors, m.name AS Movie, j.role AS Role FROM actors a LEFT JOIN acted_in j ON a.id = j.actor_id LEFT JOIN movies m ON j.movie_id = m.id"
+results = sqlContext.sql(s)
+for result in results.collect():
+    print(result)
